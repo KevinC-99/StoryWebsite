e04f1069d65a7e6767aa831411075d53f83843b2
holly-hewitt <holly-hewitt@hotmail.co.uk>
2023-10-08 16:45:30 +0100

    Update views.py

--- stories/views.py
+++ stories/views.py
@@ -8,3 +8,23 @@ def story_list(request):
 def story_detail(request, pk):
     story = get_object_or_404(Story, pk=pk)
     return render(request, 'stories/story_detail.html', {'story': story})
+
+def app_home(request):
+    # Add your app home logic here
+    return render(request, 'app_home.html')
+
+def chat_home(request):
+    # Add your chat home logic here
+    return render(request, 'chat_home.html')
+
+def story_home(request):
+    # Add your story home logic here
+    return render(request, 'story_home.html')
+
+def story_page_view(request):
+    # Add your story page view logic here
+    return render(request, 'story_page_view.html')
+
+def story_view(request):
+    # Add your story view logic here
+    return render(request, 'story_view.html')
